49b919508baef091b31b1f0d0b06740fc0b232c4
Sean Cafe <seanamir30@gmail.com>
2022-07-29 01:41:55 +0800

    added is_admin on sanitizer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,7 +48,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :is_admin])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
